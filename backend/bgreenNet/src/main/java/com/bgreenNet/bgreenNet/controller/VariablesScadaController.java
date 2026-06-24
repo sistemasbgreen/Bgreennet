a261234b5a7ba27b5d6773585e1c5f02d18daaec
JoseAngulo <asistentetic@bgreen.com.co>
2026-06-24 16:20:38 -0500

    [Bgreen-115][Bgreen-120][Bugs] Configuracion de formula

--- backend/bgreenNet/src/main/java/com/bgreenNet/bgreenNet/controller/VariablesScadaController.java
+++ backend/bgreenNet/src/main/java/com/bgreenNet/bgreenNet/controller/VariablesScadaController.java
@@ -1,15 +1,19 @@
 package com.bgreenNet.bgreenNet.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @RequestMapping({"/api/scada", "/scada"})
-@CrossOrigin(origins = "*") // luego puedes restringir a tu Angular
+@CrossOrigin(origins = "*")
 public class VariablesScadaController {
 
 
